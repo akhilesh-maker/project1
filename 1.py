ec1bd69e37c36f4be1708a702e057f28d9b39e9c
akhilesh-maker <151713531+akhilesh-maker@users.noreply.github.com>
2024-10-16 15:08:52 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,14 @@
+#parameters:placeholder
+#arguments: actual values
+#positional
+#keyword
+def addnumber(a,b): 
+    return a+b
+result=addnumber(a: 4,b: 2)
+print(result)
+
+
+def greet(name,age):
+    print(f"hello,{name},your age is {age}")
+greet(age=30,name="ram")
+
